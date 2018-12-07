7d4d758bea83e7e1010d301b5a6bb7f77dd23f44
ikkacu <evanmliu@gmail.com>
2018-12-06 19:21:11 -0500

    need

---
+++ Assets/AnimationBean.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationBean {
+    public string EnemyAttack { get; private set; }
+    public string CharAttack { get; private set; }
+    public bool Win { get; private set; }
+    public Animatable animatable { get; private set; }
+    public AnimationBean (string enemyAttackInit, string charAttackInit, bool winInit, Animatable animatableInit) {
+        EnemyAttack = enemyAttackInit;
+        CharAttack = charAttackInit;
+        Win = winInit;
+        animatable = animatableInit;
+    }
+}
